d2f0e7cf9c01c38dead0727c84bd35cd9c263589
viktor sapa <sapa.viktor.nik@gmail.com>
2026-05-23 20:18:04 +0700

    add README.md

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     print(mask_account_card("Visa Platinum 8990922113665229"))
     print(mask_account_card("Visa Gold 5999414228426353"))
     print(mask_account_card("Счет 73654108430135874305"))
-
+    print(get_date("2024-03-11T02:26:18.671407"))
     print(
         filter_by_filter_by_state(
             [
