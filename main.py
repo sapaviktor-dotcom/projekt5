5a5b1a29fa05f5e1c88c4c977ddf20303338c866
viktor sapa <sapa.viktor.nik@gmail.com>
2026-05-30 18:12:55 +0700

    add test_masks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date, mask_account_card
 from src.processing import filter_by_filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
 
@@ -22,7 +22,9 @@ if __name__ == "__main__":
                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 615064591,
+                 "state": "CANCELED",
+                 "date": "2018-10-14T08:21:33.419441"},
             ]
         )
     )
--- src/masks.py
+++ src/masks.py
@@ -1,9 +1,19 @@
 def get_mask_account(account_number: str) -> str:
     """Функция  принимает на вход номер счета в виде строки и возвращает маску номера по правилу **XXXX"""
 
+    if not isinstance(account_number, str):
+        raise ValueError("Ввод должен быть строкой")
+
+    if not  account_number.isdigit() or len(account_number) < 4:
+        raise ValueError(" Не корректный ввод")
+
+
     # Преобразуем в строку на случай, если пришло число
 
     account_str = str(account_number)
+
+    if len(account_str) != 20:
+        raise ValueError("Номер счёта должен содержать 20 цифр")
 
     # Берем последние 4 цифры и добавляем две звездочки перед ними
 
@@ -14,12 +24,25 @@ def get_mask_card_number(card_number: str) -> str:
     """
     Функция принимает на вход номер карты в виде строки и возвращает маску номера по правилу XXX XX** **** XXXX
     """
+    if not isinstance(card_number, str):
+        raise ValueError("Ввод должен быть строкой")
 
+    if not card_number.isdigit() :
+        raise ValueError(" Не корректный ввод")
     # Превращаем в строку, если пришло число
     card_str = str(card_number)
+
+    # Проверяем длину номера карты
+    if len(card_str) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр")
 
     # Формируем маску: первые 6 цифр, звезды и последние 4
 
     mask = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
 
     return mask
+
+
+
+
+
